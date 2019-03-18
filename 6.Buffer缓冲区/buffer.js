68b91ebdd14d3acf8e68e988614e71e52647bfa2
Drafffffff <i0.0ia@qq.com>
2019-03-18 13:14:20 +0800

    看你是

---
+++ 6.Buffer缓冲区/buffer.js
@@ -0,0 +1,14 @@
+//创建BUffer实例
+
+var buf1 = new Buffer(10);
+
+var buf2 = new Buffer([10,20,30,40,50]);
+
+var buf3 = new Buffer("drafffffffffff","utf-8");
+
+
+var buf4 = new Buffer(256);
+
+len = buf4.write("sdfsdfsdfsdfsdf");
+console.log("写入的字节数："+len)
+
